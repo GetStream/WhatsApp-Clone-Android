b76f7c3013a17e96f6a51d52a2c2e36882fec190
Thierry Schellenbach <thierryschellenbach@gmail.com>
2020-01-24 20:47:34 -0700

    improve the custom viewholder

--- app/src/main/java/com/example/whatsappclone/ui/channel_list/CustomViewHolderFactory.kt
+++ app/src/main/java/com/example/whatsappclone/ui/channel_list/CustomViewHolderFactory.kt
@@ -2,38 +2,28 @@ package com.example.whatsappclone.ui.channel_list
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import com.getstream.sdk.chat.MarkdownImpl
 import com.getstream.sdk.chat.adapter.BaseChannelListItemViewHolder
 import com.getstream.sdk.chat.adapter.ChannelListItemAdapter
 import com.getstream.sdk.chat.adapter.ChannelViewHolderFactory
-import com.getstream.sdk.chat.model.Channel
 
+
+/*
+Make the view holder factory return CustomChannelListItemViewHolder
+ */
 class CustomViewHolderFactory : ChannelViewHolderFactory() {
-
-    private val CHANNEL_GENERAL = 0
-
-    override fun getChannelViewType(channel: Channel?): Int {
-        return CHANNEL_GENERAL
-    }
-
     override fun createChannelViewHolder(
         adapter: ChannelListItemAdapter,
         parent: ViewGroup,
         viewType: Int
-    ): BaseChannelListItemViewHolder? {
-        return if (viewType == CHANNEL_GENERAL) {
-            val style = adapter.style
-            val v = LayoutInflater.from(parent.context)
-                .inflate(style.channelPreviewLayout, parent, false)
-            val holder = CustomChannelListItemViewHolder(v)
-            holder.setStyle(style)
-            holder.setMarkdownListener(MarkdownImpl.getMarkdownListener())
-            holder.setChannelClickListener(adapter.channelClickListener)
-            holder.setChannelLongClickListener(adapter.channelLongClickListener)
-            holder.setUserClickListener(adapter.userClickListener)
-            holder
-        } else {
-            null
-        }
+    ): BaseChannelListItemViewHolder? { // get the style object
+        val style = adapter.style
+        // inflate the layout specified in the style
+        val v = LayoutInflater.from(parent.context)
+            .inflate(style.channelPreviewLayout, parent, false)
+        // configure the viewholder
+        val holder = CustomChannelListItemViewHolder(v)
+        configureHolder(holder, adapter)
+        // return..
+        return holder
     }
 }
