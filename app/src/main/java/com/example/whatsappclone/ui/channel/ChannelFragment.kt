5c065ef6b2a1f317a792c25256aee50a1c215c77
Thierry Schellenbach <thierryschellenbach@gmail.com>
2020-01-27 15:14:08 -0700

    use 3.6.3

--- app/src/main/java/com/example/whatsappclone/ui/channel/ChannelFragment.kt
+++ app/src/main/java/com/example/whatsappclone/ui/channel/ChannelFragment.kt
@@ -8,7 +8,6 @@ import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.whatsappclone.R
