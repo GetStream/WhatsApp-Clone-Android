135033141dc4df19852b722c203ea2b1592ea5ad
Jc Miñarro <josecarlos.minarro@gmail.com>
2020-05-07 16:38:32 +0200

    Create a new method to create an instance of EmptyFragment

--- app/src/main/java/com/example/whatsappclone/ui/EmptyFragment.kt
+++ app/src/main/java/com/example/whatsappclone/ui/EmptyFragment.kt
@@ -5,15 +5,20 @@ import android.view.View
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.whatsappclone.R
-import com.example.whatsappclone.ui.home.EMPTY_TITLE
-import com.example.whatsappclone.ui.home.TAB_TITLES
 
+private const val ARG_NANME = "ARG_NANME"
 class EmptyFragment : Fragment(R.layout.fragment_empty) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        arguments?.takeIf { it.containsKey(EMPTY_TITLE) }?.apply {
-            val textView: TextView = view.findViewById(R.id.text1)
-            textView.text = TAB_TITLES.getOrDefault(getInt(EMPTY_TITLE)-1, "camera")
+        view.findViewById<TextView>(R.id.text1).text = arguments?.getString(ARG_NANME)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(name: String): EmptyFragment = EmptyFragment().apply {
+            arguments = Bundle().apply {
+                putString(ARG_NANME, name)
+            }
         }
     }
 }
