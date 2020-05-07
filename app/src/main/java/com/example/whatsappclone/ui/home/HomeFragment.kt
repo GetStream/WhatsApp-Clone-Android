5a681d8e57ff416db1dab8d6b31098cb91069893
Jc Miñarro <josecarlos.minarro@gmail.com>
2020-05-07 16:41:57 +0200

    Remove MagicNumber

--- app/src/main/java/com/example/whatsappclone/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/whatsappclone/ui/home/HomeFragment.kt
@@ -1,9 +1,11 @@
 package com.example.whatsappclone.ui.home
 
 import android.os.Bundle
-import android.view.*
+import android.view.Menu
+import android.view.MenuInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.graphics.drawable.DrawableCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -16,26 +18,24 @@ import com.example.whatsappclone.ui.channel_list.ChannelListFragment
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
-
-const val EMPTY_TITLE = "empty_title"
-val TAB_TITLES = mapOf( 1 to "chats", 2 to "status", 3 to "calls")
-
+private const val INDEX_OF_CAMERA = 0
+private const val INDEX_OF_CHATS = 1
+private const val INDEX_OF_STATUS = 2
+private const val INDEX_OF_CALLS = 3
+private const val NUM_TABS = 4
+private val TAB_TITLES = mapOf(
+    INDEX_OF_CAMERA to "camera",
+    INDEX_OF_CHATS to "chats",
+    INDEX_OF_STATUS to "status",
+    INDEX_OF_CALLS to "calls"
+)
 
 class TabsAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) : FragmentStateAdapter(fragmentManager, lifecycle) {
-
-    override fun getItemCount(): Int = 4
-
-    override fun createFragment(position: Int): Fragment {
-        val fragment: Fragment
-        if (position == 1) {
-            fragment = ChannelListFragment()
-        } else {
-            fragment = EmptyFragment()
-            fragment.arguments = Bundle().apply {
-                putInt(EMPTY_TITLE, position + 1)
-            }
-        }
-        return fragment
+    override fun getItemCount(): Int = NUM_TABS
+    override fun createFragment(position: Int): Fragment = if (position == INDEX_OF_CHATS) {
+        ChannelListFragment()
+    } else {
+        EmptyFragment.newInstance(TAB_TITLES[position] ?: "")
     }
 }
 
@@ -63,20 +63,19 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         activity.setSupportActionBar(toolbar)
 
         val tabLayout : TabLayout = fragmentView.findViewById(R.id.tabs)
-        val chatTab: TabLayout.Tab = tabLayout.getTabAt(2)!!
         viewPager = fragmentView.findViewById(R.id.view_pager)
         viewPager.adapter = TabsAdapter(childFragmentManager, lifecycle)
 
         // connect the tabs and view pager2
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-            tab.text = TAB_TITLES[position]
-            if (position == 0) {
+            if (position == INDEX_OF_CAMERA) {
                 tab.setIcon(R.drawable.ic_camera_alt_black_24dp)
+            } else {
+                tab.text = TAB_TITLES[position]
             }
             viewPager.setCurrentItem(tab.position, true)
         }.attach()
-        tabLayout.selectTab(chatTab)
-
+        tabLayout.getTabAt(INDEX_OF_CHATS)?.let { tabLayout.selectTab(it) }
         // handle tint for the camera icon
         val colors = resources.getColorStateList(R.color.tab_icon, activity.theme)
 
